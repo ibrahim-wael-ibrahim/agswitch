7a87de4ed6d47189b67ea763794c3f34ec516df2
ibrahim wael ibrahim fouad <168570569+ibrahim-wael-ibrahim@users.noreply.github.com>
2026-07-24 23:04:29 +0300

    feat: rotate Antigravity logs before launch

--- internal/process/launcher_linux.go
+++ internal/process/launcher_linux.go
@@ -16,7 +16,8 @@ import (
 )
 
 type LinuxLauncher struct {
-	LogPath string
+	LogPath     string
+	MaxLogBytes int64
 }
 
 func (l LinuxLauncher) Launch(ctx context.Context, executable string, args ...string) error {
@@ -34,6 +35,9 @@ func (l LinuxLauncher) Launch(ctx context.Context, executable string, args ...st
 	var output io.WriteCloser
 	if l.LogPath != "" {
 		if err := fsutil.EnsurePrivateDir(filepath.Dir(l.LogPath)); err != nil {
+			return err
+		}
+		if err := l.rotateLog(); err != nil {
 			return err
 		}
 		file, err := os.OpenFile(l.LogPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o600)
@@ -63,4 +67,29 @@ func (l LinuxLauncher) Launch(ctx context.Context, executable string, args ...st
 		_ = output.Close()
 	}()
 	return nil
+}
+
+func (l LinuxLauncher) rotateLog() error {
+	maxBytes := l.MaxLogBytes
+	if maxBytes <= 0 {
+		maxBytes = 5 << 20
+	}
+	info, err := os.Stat(l.LogPath)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil
+	}
+	if err != nil {
+		return fmt.Errorf("stat Antigravity log: %w", err)
+	}
+	if info.Size() <= maxBytes {
+		return nil
+	}
+	backup := l.LogPath + ".old"
+	if err := os.Remove(backup); err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("remove old log backup: %w", err)
+	}
+	if err := os.Rename(l.LogPath, backup); err != nil {
+		return fmt.Errorf("rotate Antigravity log: %w", err)
+	}
+	return os.Chmod(backup, 0o600)
 }
